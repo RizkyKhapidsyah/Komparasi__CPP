d47b3bc12801b92e940a64c36417543b9bfb23f5
Rizky Hafitsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2019-07-09 20:07:20 +0700

    Add files via upload

---
+++ Inisialisasi.c
@@ -0,0 +1,23 @@
+
+
+#define _NL endl;
+#define __NL "\n" << endl;
+#define __pNL cout << "\n"
+#define T1 "Inisialisasi:"
+#define sT1 "Nilai "
+#define sTp1 "Operasi Komparasi: 'Sebanding'. Lambang: =="
+#define sTp2 "Operasi Komparasi: 'Tidak Sebanding'. Lambang: !="
+#define sTp3 "Operasi Komparasi: 'Kurang Dari'. Lambang: <"
+#define sTp4 "Operasi Komparasi: 'Lebih Dari'. Lambang: >"
+#define sTp5 "Operasi Komparasi: 'Kurang Dari Sama Dengan'. Lambang: >="
+#define sTp6 "Operasi Komparasi: 'Lebih Dari Sama Dengan'. Lambang: <="
+#define Baris "-"
+
+void TampilkanNilai();
+void cBaris();
+void aBaris();
+void p_cHasil();
+void Char_C();
+
+int a = 6;
+int b = 5;
---
+++ Komparasi.cpp
@@ -0,0 +1,95 @@
+/*
+	Source Behind: Kelas Terbuka
+	Modified by: Rizky Khapidsyah
+
+	For general learning.
+*/
+#include <iostream>
+#include "Inisialisasi.c"
+
+using namespace std;
+
+bool hasil;
+string C;
+
+
+int main()
+{
+	TampilkanNilai();
+
+	//Komparasi: Sebanding. Lambang: ==
+	cBaris(); cout << sTp1 << endl; cBaris();
+	hasil = (a == b);
+	cout << "(a == b): " << hasil << endl;
+	p_cHasil(); Char_C(); aBaris();
+
+	//Komparasi: Tidak Sebanding. Lambang: !=
+	cBaris(); cout << sTp2 << endl; cBaris();
+	hasil = (a != b);
+	cout << "(a != b): " << hasil << endl;
+	p_cHasil(); Char_C(); aBaris();
+
+	//Komparasi: Kurang Dari. Lambang: <
+	cBaris(); cout << sTp3 << endl; cBaris();
+	hasil = (a < b);
+	cout << "(a < b): " << hasil << endl;
+	p_cHasil(); Char_C(); aBaris();
+
+	//Komparasi: Lebih Dari. Lambang: >
+	cBaris(); cout << sTp4 << endl; cBaris();
+	hasil = (a > b);
+	cout << "(a > b): " << hasil << endl;
+	p_cHasil(); Char_C(); aBaris();
+
+	//Komparasi: Kurang Dari Sama Dengan. Lambang: <=
+	cBaris(); cout << sTp5 << endl; cBaris();
+	hasil = (a <= b);
+	cout << "(a <= b): " << hasil << endl;
+	p_cHasil(); Char_C(); aBaris();
+
+	//Komparasi: Lebih Dari Sama Dengan. Lambang: >=
+	cBaris(); cout << sTp6 << endl; cBaris();
+	hasil = (a >= b);
+	cout << "(a >= b): " << hasil << endl;
+	p_cHasil(); Char_C(); aBaris();
+
+	cin.get();
+	return 0;
+}
+
+void TampilkanNilai()
+{
+	cout << T1 << _NL;
+	cout << "# " << sT1 << "a: " << a << _NL;
+	cout << "# " << sT1 << "b: " << b << __NL;
+}
+
+void cBaris()
+{
+	for (int i = 0; i <= 60; i++)
+	{
+		cout << Baris;
+	}
+	cout << endl;
+}
+
+void aBaris()
+{
+	cout << endl;  __pNL;
+}
+
+void p_cHasil()
+{
+	switch (hasil)
+	{
+	case 0: C = "False";
+		break;
+	case 1: C = "True";
+		break;
+	}
+}
+
+void Char_C()
+{
+	cout << C << endl;
+}
